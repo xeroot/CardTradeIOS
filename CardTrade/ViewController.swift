477f9d106985477b5d190c9cbfdb23c40131ed0a
xer0 <xer0@xer0s-mac.local>
2018-06-23 08:46:23 -0700

    Initial Commit

---
+++ CardTrade/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CardTrade
+//
+//  Created by xer0 on 14/06/18.
+//  Copyright © 2018 xer0. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'CardTrade' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for CardTrade
+
+    pod 'SwiftyJSON', '~> 4.0'
+    pod 'Alamofire', '~> 4.7'
+
+  target 'CardTradeTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'CardTradeUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
