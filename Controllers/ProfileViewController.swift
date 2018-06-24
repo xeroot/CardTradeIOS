b4c77128295d49f710eafa69f5b4ebe797bbf02e
xer0 <xer0@xer0s-mac.local>
2018-06-24 00:01:53 -0700

    xd

---
+++ Controllers/ProfileViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ProfileViewController.swift
+//  CardTrade
+//
+//  Created by Bridge xd on 23/06/18.
+//  Copyright © 2018 xer0. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class ProfileViewController: UIViewController {
+
+  
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPhone: UITextField!
+    @IBOutlet weak var txtAge: UITextField!
+    @IBOutlet weak var txtGender: UITextField!
+    @IBOutlet weak var txtCoin: UITextField!
+    @IBOutlet weak var txtRating: UITextField!
+    @IBOutlet weak var txtAddress: UITextField!
+    let myProfile = profile()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let userDefaults = UserDefaults.standard
+        let userId = userDefaults.integer(forKey: "UserId")
+        print(userId)
+        Alamofire.request("http://192.168.1.2:45455/api/Profiles/" + String(userId)).responseJSON { response in
+            if let json = response.result.value {
+                print("JSON: \(json)") // serialized json response
+                //Read json
+                let sJson = JSON(json)
+                print(sJson["Name"].stringValue)
+                self.myProfile.Name = sJson["Name"].stringValue
+                self.myProfile.Email = sJson["Email"].stringValue
+                self.myProfile.Phone = sJson["Phone"].stringValue
+                self.myProfile.Age = sJson["Age"].stringValue
+                self.myProfile.Sex = sJson["Sex"].stringValue
+                self.myProfile.Coins = sJson["Coins"].intValue
+                self.myProfile.Rating = sJson["Rating"].doubleValue
+                self.myProfile.Address = sJson["Address"].stringValue
+                
+                self.txtName.text =  self.myProfile.Name
+                self.txtEmail.text =  self.myProfile.Email
+                self.txtPhone.text = self.myProfile.Phone
+                self.txtAge.text = self.myProfile.Age
+                self.txtGender.text = self.myProfile.Sex
+                self.txtCoin.text = String(self.myProfile.Coins)
+                self.txtRating.text = String(self.myProfile.Rating)
+                self.txtAddress.text = self.myProfile.Address
+            }
+        }
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        
+        
+        // Do any additional setup after loading the view.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Entities/profile.swift
@@ -0,0 +1,23 @@
+//
+//  Profile.swift
+//  CardTrade
+//
+//  Created by Bridgexd on 23/06/18.
+//  Copyright © 2018 xer0. All rights reserved.
+//
+
+import UIKit
+
+class profile: NSObject {
+
+    var Name : String = ""
+    var Email : String = ""
+    var Phone : String = ""
+    var Age : String = ""
+    var Sex : String = ""
+    var Coins : Int = 0
+    var Rating : Double = 0.0
+    var Address : String = ""
+    
+  
+}
