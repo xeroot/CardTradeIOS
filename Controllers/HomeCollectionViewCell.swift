db7b263a38a38eabb25939bb7c93c0f0e821f122
xer0 <xer0@xer0s-mac.local>
2018-06-24 22:33:30 -0700

    Agregado por Jorge

---
+++ Controllers/HomeCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  HomeCollectionViewCell.swift
+//  CardTrade
+//
+//  Created by xer0 on 24/06/18.
+//  Copyright © 2018 xer0. All rights reserved.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var lblItem: UILabel!
+    @IBOutlet weak var lblPrice: UILabel!
+    @IBOutlet weak var lblTime: UILabel!
+    @IBOutlet weak var imgItem: UIImageView!
+}
---
+++ Entities/Auction.swift
@@ -0,0 +1,23 @@
+//
+//  Auction.swift
+//  CardTrade
+//
+//  Created by xer0 on 24/06/18.
+//  Copyright © 2018 xer0. All rights reserved.
+//
+
+import UIKit
+
+class Auction: NSObject {
+    var calificationUser : Double = 0.0
+    var userId : Int = 0
+    var cardNane : String  = ""
+    var userNameUserSeller : String = ""
+    var currentAmount : Double = 0.0
+    var beginDate : String = ""
+    var endDate : String = ""
+    var descriptionCard : String = ""
+    var status : String = ""
+    var type : String = ""
+    var amount : String = ""
+}
